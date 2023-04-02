d794779e8fdd2cbbd3ea9def552c686e1d5d4993
Adam <adamb56789@gmail.com>
2023-04-02 23:48:35 +0100

    Refactoring, tweaks, add GenerateGraph, add IoT rule

--- lib/iot_system_cdk-stack.ts
+++ lib/iot_system_cdk-stack.ts
@@ -7,24 +7,253 @@ import { join } from 'path'
 import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 import { Topic } from 'aws-cdk-lib/aws-sns';
 import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Runtime, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
+import { PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Runtime, FunctionUrlAuthType, LayerVersion, Function } from 'aws-cdk-lib/aws-lambda';
 import { AwsCustomResource, PhysicalResourceId, AwsCustomResourcePolicy } from 'aws-cdk-lib/custom-resources';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha'
+import { CfnDatabase, CfnTable } from 'aws-cdk-lib/aws-timestream';
+import { CfnTopicRule } from 'aws-cdk-lib/aws-iot';
 
+
+const AWS_PANDAS_LAYER_ARN = "arn:aws:lambda:eu-west-1:336392948345:layer:AWSSDKPandas-Python37:1"
+const MATPLOTLIB_LAYER_ARN = "arn:aws:lambda:eu-west-1:686637384519:layer:Matplotlib_Layer:2"
+const CORRECT_PASSWORD_HASH_PARAMETER = '/PicoTherm/CorrectPasswordHash';
 
 export class IotSystemCdkStack extends Stack {
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const snsTopic = new Topic(this, 'IoTSystemErrors')
+    this.createLatestMeasurementsResources(snsTopic)
+    this.createHistoryGraphResources(snsTopic)
+  }
+
+  /**
+   * Create all of the resources for getting the latest measurements
+   * @param topic SNS topic for receiving notifications
+   */
+  private createLatestMeasurementsResources(topic: Topic) {
+    // Get the URL of the IOT Core broker endpoint
+    const IOT_ENDPOINT = this.getIotEndpoint();
+
+    const locationTable = this.createLocationTable();
+
+    const getLatestMeasurementsLambda = this.createLatestMeasurementsLambda(IOT_ENDPOINT, locationTable);
+
+    // Alarm on errors and send emails
+    const passwordAlarm = this.createOnErrorAlarm(
+      getLatestMeasurementsLambda.logGroup,
+      "GetLatestMeasurementsIncorrectPassword",
+      "Incorrect password"
+    )
+    const parameterAlarm = this.createOnErrorAlarm(
+      getLatestMeasurementsLambda.logGroup,
+      "GetLatestMeasurementsIncorrectParameters",
+      "Incorrect parameters"
+    )
+    passwordAlarm.addAlarmAction(new SnsAction(topic))
+    parameterAlarm.addAlarmAction(new SnsAction(topic))
+  }
+
+  /**
+   * Create all of the resources for viewing measurement history graphs
+   * @param topic SNS topic for receiving notifications
+   */
+  private createHistoryGraphResources(topic: Topic) {
+    const measurementsTable = this.createTimestreamTable()
+    const graphLambda = this.createHistoryGraphLambda(measurementsTable)
+
+    this.createIotTimestreamRule(measurementsTable)
+
+    // Alarm on errors and send emails
+    const passwordAlarm = this.createOnErrorAlarm(
+      graphLambda.logGroup,
+      "GenerateHistoryGraphIncorrectPassword",
+      "Incorrect password"
+    )
+    passwordAlarm.addAlarmAction(new SnsAction(topic))
+  }
+
+  /**
+   * Create a rule which sends picotherm data from IoT to Timestream
+   * @param measurementsTable a Timestream CfnTable to insert measurements into
+   * @returns a CfnTopicRule that sends data to timestream
+   */
+  private createIotTimestreamRule(measurementsTable: CfnTable): CfnTopicRule {
+    const role = new Role(this, "IotTimestreamAccessRole", {
+      assumedBy: new ServicePrincipal("iot.amazonaws.com")
+    })
+
+    role.addToPolicy(new PolicyStatement({
+      actions: ['timestream:WriteRecords', 'timestream:DescribeEndpoints'],
+      resources: [measurementsTable.attrArn]
+    }))
+
+    return new CfnTopicRule(this, 'PicothermTimestreamRule', {
+      topicRulePayload: {
+        actions: [{
+          timestream: {
+            databaseName: measurementsTable.databaseName,
+            dimensions: [{
+              name: 'device',
+              value: '${topic()}',
+            }],
+            roleArn: role.roleArn,
+            tableName: measurementsTable.attrName
+          },
+        }],
+        sql: 'SELECT temperature, humidity FROM "picotherm/#"',
+      }
+    });
+  }
+
+  /**
+   * Creates a Timstream database with a table containing measurements from the devices
+   * @returns a measurements Timestream CfnTable
+   */
+  private createTimestreamTable(): CfnTable {
+    const databaseName = "IOT_DB";
+    const database = new CfnDatabase(this, "IOT_DB", {
+      databaseName: databaseName
+    })
+
+    const table = new CfnTable(this, "measurements", {
+      databaseName: databaseName,
+      tableName: "measurements",
+      magneticStoreWriteProperties: { EnableMagneticStoreWrites: false },
+      retentionProperties: { MemoryStoreRetentionPeriodInHours: '1', MagneticStoreRetentionPeriodInDays: '21900' }
+    })
+    table.addDependency(database)
+    table.addOverride('DeletionPolicy', 'Retain')
+    database.addOverride('DeletionPolicy', 'Retain')
+
+    return table
+  }
+
+  /**
+   * Python function returning a webpage with a graph displaying historical
+   * measurement data from Timestream
+   * @returns a lambda function
+   */
+  private createHistoryGraphLambda(measurementsTable: CfnTable): Function {
+    const awsPandasLayer = LayerVersion.fromLayerVersionArn(this, "PandasLayer", AWS_PANDAS_LAYER_ARN);
+    const myMatplotlibLayer = LayerVersion.fromLayerVersionArn(this, "MatplotlibLayer", MATPLOTLIB_LAYER_ARN);
+
+    const graphLambda = new PythonFunction(this, 'generateHistoryGraph', {
+      entry: join(__dirname, 'lambdas', 'GenerateHistoryGraph'),
+      runtime: Runtime.PYTHON_3_7,
+      layers: [awsPandasLayer, myMatplotlibLayer],
+      environment: {
+        CORRECT_PASSWORD_HASH: StringParameter.valueFromLookup(this, CORRECT_PASSWORD_HASH_PARAMETER),
+        TIMESTREAM_DB_NAME: measurementsTable.databaseName,
+        TIMESTREAM_TABLE_NAME: measurementsTable.attrName
+      },
+      memorySize: 1000
+    });
+
+    // Need access to query Timestream
+    graphLambda.addToRolePolicy(new PolicyStatement({
+      actions: ['timestream:Select', 'timestream:DescribeEndpoints'],
+      resources: ['*']
+    }));
+
+    // Generate and output the function URL
+    const graphLambdaUrl = graphLambda.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+    new CfnOutput(this, 'graphLambdaUrl', {
+      value: graphLambdaUrl.url,
+    });
+
+    return graphLambda
+  }
+
+  /**
+   * Table containing mapping between device id and location
+   * @returns DynamoDB table
+   */
+  private createLocationTable(): Table {
+    return new Table(this, 'DeviceLocations', {
+      partitionKey: {
+        name: 'device_id',
+        type: AttributeType.STRING
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+  }
+
+  /**
+   * Node.js function returning a webpage with data from a list of devices
+   * @param iotEndpoint the URL of the IoT core endpoint
+   * @param locationTable a DynamoDB table mapping device ID to location
+   * @returns a lambda function
+   */
+  private createLatestMeasurementsLambda(iotEndpoint: string, locationTable: Table): Function {
+    const lambdaFunction = new NodejsFunction(this, 'getLatestMeasurements', {
+      entry: join(__dirname, 'lambdas', 'GetLatestMeasurements', 'index.js'),
+      bundling: {
+        externalModules: [
+          'aws-sdk',
+        ],
+      },
+      environment: {
+        IOT_ENDPOINT: iotEndpoint,
+        TABLE_NAME: locationTable.tableName,
+        PASSWORD_HASH: StringParameter.valueFromLookup(this, CORRECT_PASSWORD_HASH_PARAMETER)
+      },
+      runtime: Runtime.NODEJS_14_X,
+      memorySize: 1000
+    });
+
+    // Lambda needs perms for location table and IoT
+    locationTable.grantReadData(lambdaFunction)
+    lambdaFunction.addToRolePolicy(new PolicyStatement({
+      actions: ['iot:GetRetainedMessage'],
+      resources: ['*']
+    }));
+
+    // Generate and output the function URL
+    const getLatestMeasurementsUrl = lambdaFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+    new CfnOutput(this, 'getLatestMeasurementsUrl', {
+      value: getLatestMeasurementsUrl.url,
+    });
+
+    return lambdaFunction
+  }
+
+  /**
+   * Gets the endpoint of the IoT Core broker for accessing the measurements
+   * @returns the endpoint URL
+   */
+  private getIotEndpoint(): string {
+    const getIoTEndpoint = new AwsCustomResource(this, 'IoTEndpoint', {
+      onCreate: {
+        service: 'Iot',
+        action: 'describeEndpoint',
+        physicalResourceId: PhysicalResourceId.fromResponse('endpointAddress'),
+        parameters: {
+          "endpointType": "iot:Data-ATS"
+        }
+      },
+      policy: AwsCustomResourcePolicy.fromSdkCalls({ resources: AwsCustomResourcePolicy.ANY_RESOURCE })
+    });
+    const IOT_ENDPOINT = getIoTEndpoint.getResponseField('endpointAddress')
+    return IOT_ENDPOINT
+  }
 
   /**
    * Scan the logs with a metric filter, set an alarm to send to SNS if anything
    * is found
-   * 
    * @param logGroup logs from a lambda function
    * @param metricName name of the metric, also used to construct Alarm name
    * @param errorString the string to search for in the logs for the error
    * @returns an Alarm
    */
-  createOnErrorAlarm(logGroup: ILogGroup, metricName : string, errorString : string) : Alarm {
+  private createOnErrorAlarm(logGroup: ILogGroup, metricName: string, errorString: string): Alarm {
     const METRIC_NAMESPACE = "IoTSystem"
 
     logGroup.addMetricFilter(metricName, {
@@ -50,81 +279,5 @@ export class IotSystemCdkStack extends Stack {
       threshold: 0
     })
     return alarm
-  }
-
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    // Get the URL of the IOT Core broker endpoint
-    const getIoTEndpoint = new AwsCustomResource(this, 'IoTEndpoint', {
-      onCreate: {
-        service: 'Iot',
-        action: 'describeEndpoint',
-        physicalResourceId: PhysicalResourceId.fromResponse('endpointAddress'),
-        parameters: {
-          "endpointType": "iot:Data-ATS"
-        }
-      },
-      policy: AwsCustomResourcePolicy.fromSdkCalls({resources: AwsCustomResourcePolicy.ANY_RESOURCE})
-    });
-    const IOT_ENDPOINT = getIoTEndpoint.getResponseField('endpointAddress')
-
-    // Table containing mapping between device id and location
-    const locationTable = new Table(this, 'DeviceLocations', {
-      partitionKey: {
-        name: 'device_id',
-        type: AttributeType.STRING
-      },
-      billingMode: BillingMode.PAY_PER_REQUEST
-    });
-
-    // Node lambda returning webpage with data from a list of devices
-    const getLatestMeasurementsLambda = new NodejsFunction(this, 'getLatestMeasurements', {
-      entry: join(__dirname, 'lambdas', 'GetLatestMeasurements', 'index.js'),
-      bundling: {
-        externalModules: [
-          'aws-sdk',
-        ],
-      },
-      environment: {
-        IOT_ENDPOINT: IOT_ENDPOINT,
-        TABLE_NAME: locationTable.tableName,
-        PASSWORD_HASH: StringParameter.valueFromLookup(this, '/PicoTherm/CorrectPasswordHash')
-      },
-      runtime: Runtime.NODEJS_14_X,
-      memorySize: 1000
-    });
-
-    // Lambda needs perms for location table and IoT
-    locationTable.grantReadData(getLatestMeasurementsLambda)
-    getLatestMeasurementsLambda.addToRolePolicy(new PolicyStatement({
-      actions: ['iot:GetRetainedMessage'],
-      resources: ['*']
-    }));
-
-    // Generate and output the function URL
-    const getLatestMeasurementsUrl = getLatestMeasurementsLambda.addFunctionUrl({
-      authType: FunctionUrlAuthType.NONE,
-    });
-    new CfnOutput(this, 'getLatestMeasurementsUrl', {
-      value: getLatestMeasurementsUrl.url,
-    });
-
-    // Alarm on errors and send emails
-    const passwordAlarm = this.createOnErrorAlarm(
-      getLatestMeasurementsLambda.logGroup,
-      "GetLatestMeasurementsIncorrectPassword",
-      "Incorrect password"
-    )
-    const parameterAlarm = this.createOnErrorAlarm(
-      getLatestMeasurementsLambda.logGroup,
-      "GetLatestMeasurementsIncorrectParameters",
-      "Incorrect parameters"
-    )
-
-    const topic = new Topic(this, 'IoTSystemErrors');
-
-    passwordAlarm.addAlarmAction(new SnsAction(topic))
-    parameterAlarm.addAlarmAction(new SnsAction(topic))
   }
 }
---
+++ lib/lambdas/GenerateHistoryGraph/index.py
@@ -0,0 +1,120 @@
+import json, io, zlib, hashlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter, HourLocator
+import os
+
+OUTPUT_FORMATS = ["raw_text_svg", "compressed_svg"]
+
+DATA_RESOLUTION = 300
+MOVAVG_RADIUS = 3
+
+CORRECT_PASSWORD_HASH = os.environ['CORRECT_PASSWORD_HASH']
+TIMESTREAM_DB_NAME = os.environ['TIMESTREAM_DB_NAME']
+TIMESTREAM_TABLE_NAME = os.environ['TIMESTREAM_TABLE_NAME']
+
+def get_query(device_name, graph_duration):
+  return f'''SELECT measure_name,
+    INTERPOLATE_LINEAR(
+      CREATE_TIME_SERIES(time, measure_value::double), 
+      SEQUENCE(min(time), max(time), (max(time) - min(time))/(300 - 1))
+    ) AS series
+  FROM "{TIMESTREAM_DB_NAME}"."{TIMESTREAM_TABLE_NAME}"
+  WHERE device='{device_name}'
+    AND time BETWEEN ago({graph_duration}) AND now()
+  GROUP BY measure_name
+  ORDER BY measure_name DESC
+  '''
+
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+def add_plot(response, device_name, time_mode, show_absolute_humidity):
+    rows = response['Rows']
+    
+    if len(rows) != 2:
+        raise Exception("Received wrong number of measures from timestream: " + str(len(rows)))
+    
+    timeseries = rows[0]['Data'][1]['TimeSeriesValue']
+    start_time = timeseries[0]['Time']
+    end_time = timeseries[-1]['Time']
+    time = pd.date_range(start_time, end_time, DATA_RESOLUTION).values
+    temperature = np.array([d['Value']['ScalarValue'] for d in rows[0]['Data'][1]['TimeSeriesValue']]).astype("single")
+    humidity = np.array([d['Value']['ScalarValue'] for d in rows[1]['Data'][1]['TimeSeriesValue']]).astype("single")
+    
+    fig, ax_t = plt.subplots(figsize=(8,5))
+    ax_rh = ax_t.twinx()
+    
+    color_t = plt.cm.viridis(0)
+    color_rh = plt.cm.viridis(0.5)
+    
+    ax_t.set_ylabel("Temperature", color=color_t)
+    ax_rh.set_ylabel("Humidity", color=color_rh)
+    
+    ax_t.plot_date(time[MOVAVG_RADIUS:-MOVAVG_RADIUS], moving_average(temperature, MOVAVG_RADIUS*2+1), "-", color=color_t, label="Temperature")
+    ax_rh.plot_date(time, humidity, '-', color=color_rh, label="Humidity")
+    
+    if show_absolute_humidity == "True":
+        absolute_humidity = (6.112*np.exp((17.67*temperature)/(temperature+243.5))*humidity*2.1674)/(273.15+temperature)
+        ax_ah = ax_t.twinx()
+        color_ah = plt.cm.viridis(.9)
+        ax_ah.set_ylabel("Absolute Humidity", color=color_ah)
+        ax_ah.plot_date(time, absolute_humidity, '-', color=color_ah, label="Absolute Humidity")
+        ax_ah.spines['right'].set_position(('outward', 60))
+        plt.title("wtf")
+        return
+    
+    if time_mode == "h":
+        ax_t.xaxis.set_major_formatter(DateFormatter('%H:%M'))
+        ax_t.xaxis.set_minor_locator(HourLocator())
+    elif time_mode == "d":
+        ax_t.xaxis.set_major_formatter(DateFormatter('%m-%d'))
+    
+    ax_t.grid(axis='x', which='both', linestyle='--')
+    
+
+    plt.title(device_name)
+    plt.tight_layout()
+
+def check_param(param, valid):
+    if param not in valid:
+        raise ValueError(f"Format incorrect: {param} not in {valid}")
+
+import boto3
+client = boto3.client('timestream-query')
+
+def handler(event, context):
+    # If is a lambda url extract the url params
+    if 'queryStringParameters' in event:
+        event = event['queryStringParameters']
+    
+    password = event['password']
+    output_format = event['output_format']
+    device_name = event['device_name']
+    graph_duration = event['graph_duration']
+    show_absolute_humidity = event['show_absolute_humidity']
+    
+    hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    
+    if hash != CORRECT_PASSWORD_HASH:
+        raise Exception("Incorrect password")
+
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    check_param(output_format, OUTPUT_FORMATS)
+        
+    response = client.query(QueryString=get_query(device_name, graph_duration))
+    
+    add_plot(response, device_name, graph_duration[-1], show_absolute_humidity)
+
+    f = io.BytesIO()
+    plt.savefig(f, format = "svg")
+    plt.close()
+    
+    raw_svg = f.getvalue()
+    
+    if output_format == "raw_text_svg":
+        return raw_svg.decode("utf-8")
+    else:
+        return zlib.compress(raw_svg)
